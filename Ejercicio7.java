289310405dcbe9c5548465a7231455fa23d09a41
Clb184 <84605945+Clb184@users.noreply.github.com>
2023-06-13 20:24:09 -0500

    Add files via upload

---
+++ Ejercicio7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.pe.utp.laboratorio12;
+
+/**
+ *
+ * @author sebas
+ */
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        CCar car = new CCar();
+        car.calcPrice();
+        System.out.print("El precio final del auto es: "); car.printFinalPrice(); System.out.print(" soles.\n");
+    }
+}
